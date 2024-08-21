ec279fa2b9024a9d708b3a56a04a257d70b4e7e5
Limbert <infolimberleon@gmail.com>
2024-08-21 17:13:15 -0400

    simplificacion del manejo de fragment y uso de permisos en el locationfragment

--- app/src/main/java/com/example/aquisito/LocationFragment.kt
+++ app/src/main/java/com/example/aquisito/LocationFragment.kt
@@ -1,19 +1,133 @@
 package com.example.aquisito
 
+import android.Manifest
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Looper.getMainLooper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.aquisito.databinding.FragmentLocationBinding
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
 
 class LocationFragment : Fragment() {
 
+    private lateinit var locationBinding: FragmentLocationBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                checkAndEnableGPS()
+            } else {
+                locationBinding.tvLocation.text = getString(R.string.permiso_denegado)
+            }
+
+        }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        locationBinding = FragmentLocationBinding.inflate(inflater, container, false)
+        return locationBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Verificar permisos de ubicación
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // Permiso ya concedido, habilitar GPS
+                checkAndEnableGPS()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                // Mostrar una UI educativa para explicar por qué se necesita el permiso
+                showInContextUI()
+            }
+
+            else -> {
+                //solicitar permiso
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+
+    }
+
+
+    private fun checkAndEnableGPS() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).build()
+        val client = LocationServices.getSettingsClient(requireContext())
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        client.checkLocationSettings(builder.build()).addOnSuccessListener {
+
+            showLocationEnabledMessage()
+
+        }.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    exception.startResolutionForResult(requireActivity(), REQUEST_ENABLE_GPS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Manejar el error
+                }
+            }
+
+        }
+
+    }
+
+    private fun showLocationEnabledMessage() {
+        locationBinding.tvLocation.text = "GPS habitado"
+    }
+
+
+    private fun showInContextUI() {
+        // Aquí puedes mostrar un diálogo o una UI que explique por qué se necesita el permiso
+        // Por ejemplo, un AlertDialog explicativo
+        AlertDialog.Builder(requireContext())
+            .setTitle("Permiso de ubicacion Necesario")
+            .setMessage("Esta aplicacion ncesita acceso a la ubicacion para funcionar")
+            .setPositiveButton("OK") { _, _ ->
+                // Volver a solicitar el permiso
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
+    }
+
+    companion object {
+        private const val REQUEST_ENABLE_GPS = 2
+    }
+
 
 }
--- app/src/main/java/com/example/aquisito/MainActivity.kt
+++ app/src/main/java/com/example/aquisito/MainActivity.kt
@@ -3,78 +3,55 @@ package com.example.aquisito
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.aquisito.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     // Variable para el binding de la actividad principal
     private lateinit var mBinding: ActivityMainBinding
 
-    // Variables para manejar el fragmento activo y el administrador de fragmentos
-    private lateinit var mFragmentManager: FragmentManager
+
+    private val locationFragment = LocationFragment()
+    private val routeFragment = RouteFragment()
+    private val configFragment = ConfigFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-
-        // Configuración de la barra de herramientas (Toolbar)
         setSupportActionBar(mBinding.toolbar)
 
-        // Configurar la vista de navegación inferior
-        val bottomNavigationView = mBinding.bottomNav
 
-        // Configurar la vista de navegación inferior (BottomNavigationView)
-        setupBottomNav()
-
-        // Seleccionar y mostrar por defecto el fragmento de ubicación (LocationFragment)
-        mBinding.bottomNav.selectedItemId = R.id.action_location
-        mBinding.toolbar.title = getString(R.string.location_title)
-        supportActionBar?.title = mBinding.toolbar.title
-
-
-    }
-
-    private fun setupBottomNav(){
-
-        mFragmentManager = supportFragmentManager
-
-        val locationFragment = LocationFragment()
-        val routeFragment = RouteFragment()
-        val configFragment = ConfigFragment()
-
-        // Configuración del listener para gestionar los clics en los elementos del BottomNavigationView
-        mBinding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.action_location->{
-                    // Reemplaza el fragmento actual por el LocationFragment y actualiza el título
+        mBinding.bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_location -> {
                     replaceFragment(locationFragment, getString(R.string.location_title))
                     true
                 }
-                R.id.action_route ->{
-                    replaceFragment(routeFragment,getString(R.string.route_title))
+                R.id.action_route -> {
+                    replaceFragment(routeFragment, getString(R.string.route_title))
                     true
                 }
-                R.id.action_config->{
-                    replaceFragment(configFragment,getString(R.string.config_title))
+                R.id.action_config -> {
+                    replaceFragment(configFragment, getString(R.string.config_title))
                     true
                 }
-                else-> false
+                else -> false
             }
-
         }
+
+        // Establecer la pestaña inicial
+        mBinding.bottomNav.selectedItemId = R.id.action_location
     }
 
-    // Método para reemplazar el fragmento actual y actualizar el título de la barra de herramientas
     private fun replaceFragment(fragment: Fragment, title: String) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.hostFragment, fragment) // Reemplaza el fragmento en el contenedor hostFragment
+            .replace(R.id.hostFragment, fragment)
             .commit()
-        mBinding.toolbar.title = title  // Actualiza el título del Toolbar
-        supportActionBar?.title = title // Sincroniza el título de la ActionBar con el del Toolbar
+
+        mBinding.toolbar.title = title
+        supportActionBar?.title = title
     }
+
 }
